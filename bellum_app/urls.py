92e6bc9764a0d4bdbf5316c9b9e2ea1094781003
Santiago Chaustre <santiago.chaustreperdomo@gmail.com>
2018-05-29 16:52:53 -0500

    delete files

--- bellum_app/urls.py
+++ bellum_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register_user/', views.register),
     path('token/', views.obtain_expiring_auth_token),
     path('get_users/', views.get_users),
-    path('upload_file/', views.upload_file)
+    path('upload_file/', views.upload_file),
+    path('del_file/',views.del_file)
 ]
--- bellum_app/views.py
+++ bellum_app/views.py
@@ -9,7 +9,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
-from bellum_app.api import  user_service
+from bellum_app.api import  user_service, file_service
 from bellum_app.models import User
 from bellum_app.serializers.file_serializer import File_Serializer
 
@@ -93,8 +93,20 @@ class FileViewSet(viewsets.ViewSet):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+    def delete (self,request):
+        if file_service.delete(request.data['file_id']) :
+            return Response(
+                "Deleted ok",
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            "Not deleted",
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
 
 register =UserViewSet.as_view(dict(post='create'))
 get_users = UserViewSet.as_view(dict(get='get'))
 obtain_expiring_auth_token = ObtainExpiringAuthToken.as_view()
 upload_file = FileViewSet.as_view(dict(post='create'))
+del_file =  FileViewSet.as_view(dict(delete='delete'))
